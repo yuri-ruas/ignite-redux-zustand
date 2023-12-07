0d14716abe61bceb476a86354fa74eb228f4e39d
Yuri Ruas <yuriruas.dev@gmail.com>
2023-12-07 20:07:22 -0300

    conectando à api: carregando dados do curso

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,10 @@ import { useCurrentLesson } from "../store/slices/player"
 export function Header() {
   const { currentModule, currentLesson } = useCurrentLesson()
 
+  if (!currentLesson || !currentModule) {
+    return null
+  }
+
   return (
     <div className="flex flex-col gap-1">
       <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -6,6 +6,10 @@ export function Video() {
   const dispatch = useDispatch()
   const { currentLesson } = useCurrentLesson()
 
+  if (!currentLesson) {
+    return null
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
