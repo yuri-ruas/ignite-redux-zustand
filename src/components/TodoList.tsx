29c851fd22343da78b8f2b0653afb75452b384a4
Yuri Ruas <yuriruas.dev@gmail.com>
2023-12-07 06:53:55 -0300

    a base do redux: criando store do redux

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux'
+
+export function TodoList() {
+  const todos = useSelector(store => {
+    return store.todo
+  })
+
+  console.log(todos)
+
+  return (
+    <ul>
+      {todos.map(todo => <li key={todo}>{todo}</li>)}
+    </ul>
+  )
+}
